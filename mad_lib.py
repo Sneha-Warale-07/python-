be3e279fc6426859c2c7862903981c3e56558757
Sneha-Warale-07 <waralesneha@gmail.com>
2024-07-21 22:31:38 +0530

    mad libary added

---
+++ mad_lib.py
@@ -0,0 +1,11 @@
+def mad_libs():
+    noun = input("Enter a noun: ")
+    adjective = input("Enter an adjective: ")
+    verb = input("Enter a verb: ")
+    adverb = input("Enter an adverb: ")
+
+    print(f'''Once upon a time, there was a {adjective} {noun} who loved to {verb} {adverb}. 
+             one day he was {verb}ing , there came one lion . he fought with it fearlessly and won 
+              he  was very happy and returned to home happliy and sarted to {verb} again''')
+
+mad_libs()
